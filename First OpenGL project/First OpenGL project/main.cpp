2acd5e1cefb085e934e8f066490507b861969417
Anthony <anth19800@hotmail.com>
2019-05-01 21:13:57 -0400

    camera

--- First OpenGL project/First OpenGL project/main.cpp
+++ First OpenGL project/First OpenGL project/main.cpp
@@ -63,11 +63,16 @@ int main() {
 		
 	
 		glUseProgram(shaderProgram.getId());
+		
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		glm::mat4 view;
+		view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
 		glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		projection = glm::perspective(glm::radians(45.0f), (float)800.f / (float)600.0f, 0.1f, 100.0f);
 		// retrieve the matrix uniform locations
 		unsigned int modelLoc = glGetUniformLocation(shaderProgram.getId(), "model");
@@ -77,9 +82,9 @@ int main() {
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
 		// note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
 		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.getId(), "projection"), 1, GL_FALSE, &projection[0][0]);
-
-
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		
+		
+	//	model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		test2.draw(1);
 
 		glfwSwapBuffers(window);
